90505f1fbe2cf4401c6c8a2078d22daf52e30c67
Yashasvi Deshmukh <yashasvidesh03@gmail.com>
2026-05-16 01:30:20 +0530

    Update firebase-config.js

--- firebase/firebase-config.js
+++ firebase/firebase-config.js
@@ -1,22 +1,17 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-
-import { getAuth } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
-
+// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_BUCKET",
-  messagingSenderId: "YOUR_MESSAGE_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyC3KxWWWbiZt_PTM32ElK3T2XS-HkNRXcQ",
+  authDomain: "vrindavan-restaurant.firebaseapp.com",
+  projectId: "vrindavan-restaurant",
+  storageBucket: "vrindavan-restaurant.firebasestorage.app",
+  messagingSenderId: "505051177430",
+  appId: "1:505051177430:web:3c5deed712290c960f4477"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-const db = getFirestore(app);
-
-const auth = getAuth(app);
-
-export { db, auth };
